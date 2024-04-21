5e265bbca4c6cbe7f414397334045f64807d71e3
davidalfredostrowski <davidaostrowski@gmail.com>
2024-04-20 20:00:22 -0400

    Create AddressList.js

---
+++ uniswap/AddressList.js
@@ -0,0 +1,18 @@
+
+//https://docs.uniswap.org/contracts/v2/reference/smart-contracts/factory#:~:text=Address,%2C%20G%C3%B6rli%2C%20and%20Kovan%20testnets.
+const addressFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+//0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+
+//0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"; // WETH
+
+//0x6B3595068778DD592e39A122f4f5a5cF09C90fE2
+const addressTo = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"; // SUSHI
+
+module.exports = {
+  addressFactory,
+  addressRouter,
+  addressFrom,
+  addressTo,
+};
